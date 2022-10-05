30cccdffad89129fd234814c3ca884db5c365116
VatioN <66663300+tennus@users.noreply.github.com>
2022-10-05 14:24:21 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,71 +1,35 @@
-document.querySelector('#submit').addEventListener('click', submit)
-
-function submit(){
-  Last_name1()
-  First_name1()
-  Gender()
-  Age()
-  Nationality()
-  Last_name2()
-  First_name2()
+var db;
+function createDB() {
+  db = window.openDatabase("myDB", "1.0", "테스트용DB", 1024 * 1024);
 }
 
-function Last_name1(){
-  var Last_name1 = document.querySelector('#Last_name1').value;
-  console.log(Last_name1)
-  if (Last_name1 == "" || Last_name1 == null) {
-    alert('Write your Lastname (본인의 성을 써주세요)');
-    throw new Error("")
-  }
+function createTable() {
+  db.transaction(function (tx) {
+    tx.executeSql("create table Test(id,name)");
+  });
 }
 
-function First_name1(){
-  var First_name1 = document.querySelector('#First_name1').value;
-  console.log(First_name1)
-  if (First_name1 == "" || First_name1 == null) {
-    alert('Write your Firstname (본인의 이름을 써주세요)');
-    throw new Error("")
-  }
+function insertData() {
+  db.transaction(function (tx) {
+    tx.executeSql("insert into Test values(?,?)", [txtID.value, txtName.value]);
+  });
 }
 
-function Gender(){
-  var Gender = document.querySelector('input[name="flexRadioDefault"]:checked').value;
-  console.log(Gender)
+function deleteData() {
+  db.transaction(function (tx) {
+    tx.executeSql("delete from Test where id=?", [txtID.value]);
+  });
 }
 
-function Age(){
-  var Age = document.querySelector('#Age').value;
-  console.log(Age)
-  if (Age == "" || Age == null) {
-    alert('Write your Age (본인의 나이를 써주세요)');
-    throw new Error("")
-  }
-}
-
-function Nationality(){
-  var sel = document.querySelector('#Nationality');
-  var Nationality = sel.options[sel.selectedIndex].value;
-  console.log(Nationality)
-  if (Nationality == "Nationality (국적 선택)") {
-    alert('Check your Nationality (본인의 국적을 체크해주세요)');
-    throw new Error("")
-  }
-}
-
-function Last_name2(){
-  var Last_name2 = document.querySelector('#Last_name2').value;
-  console.log(Last_name2)
-  if (Last_name2 == "" || Last_name2 == null) {
-    alert('Write Lastname who gave the link to you (링크를 준 사람의 성을 써주세요)');
-    throw new Error("")
-  }
-}
-
-function First_name2(){
-  var First_name2 = document.querySelector('#First_name2').value;
-  console.log(First_name2)
-  if (First_name2 == "" || First_name2 == null) {
-    alert('Write Firstname who gave the link to you (링크를 준 사람의 이름을 써주세요)');
-    throw new Error("")
-  }
+function selectData() {
+  db.transaction(function (tx) {
+    tx.executeSql("select * from Test", [], function (tx, result) {
+      document.getElementById("table1").innerHTML = "";
+      for (var i = 0; i < result.rows.length; i++) {
+        var row = result.rows.item(i);
+        document.getElementById("table1").innerHTML +=
+          "<tr><td>" + row["id"] + "</td><td>" + row["name"] + "</td></tr>";
+      }
+    });
+  });
 }
